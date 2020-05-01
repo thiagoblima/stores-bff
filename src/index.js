4634f848fc235d40e9efb556177e21f0b01d92f3
Thiago Lima <thitheguy@gmail.com>
2020-05-01 17:15:55 -0300

    Adding relationship between Comments and Post  TypeDef

--- src/index.js
+++ src/index.js
@@ -91,6 +91,7 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
 
     type Comment {
@@ -171,6 +172,11 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
             })
         }
     },
