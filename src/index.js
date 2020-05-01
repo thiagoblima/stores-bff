f1ddb0d7f36f6b58b11273a383e1462e044f1956
Thiago Lima <thitheguy@gmail.com>
2020-05-01 16:53:55 -0300

    Adding relationship between Comments and  TypeDef

--- src/index.js
+++ src/index.js
@@ -43,19 +43,23 @@ const posts = [{
 const comments = [{
     id: '102',
     text: 'This worked well for me. Thanks!',
-    author: '3'
+    author: '3',
+    post: '10'
 }, {
     id: '103',
     text: 'Glad you enjoyed it.',
-    author: '1'
+    author: '1',
+    post: '11'
 }, {
     id: '104',
     text: 'This did no work.',
-    author: '2'
+    author: '2',
+    post : '11'
 }, {
     id: '105',
     text: 'Nevermind. I got it to work.',
-    author: '1'
+    author: '1',
+    post: '12'
 }]
 
 // Type definitions (schema)
@@ -93,6 +97,7 @@ const typeDefs = `
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `
 
@@ -173,6 +178,11 @@ const resolvers = {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     },
