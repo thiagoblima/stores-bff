3ae920b413c5d508f62b1610a20f1f15da3302e8
Thiago Lima <thitheguy@gmail.com>
2020-05-02 02:18:58 -0300

    Adding spread operator through babel plugin

--- src/index.js
+++ src/index.js
@@ -185,9 +185,7 @@ const resolvers = {
             
             const user = {
                 id: uuidv4(),
-                name: args.name,
-                email: args.email,
-                age: args.age
+                ...args
             }
 
             users.push(user)
@@ -203,10 +201,7 @@ const resolvers = {
             
             const post = {
                 id: uuidv4(),
-                title: args.title,
-                body: args.body,
-                published: args.published,
-                author: args.author
+                ...args 
             }
 
             posts.push(post)
@@ -223,9 +218,7 @@ const resolvers = {
 
             const comment = {
                 id: uuidv4(),
-                text: args.text,
-                author: args.author,
-                post: args.post
+                ...args
             }
 
             comments.push(comment)
