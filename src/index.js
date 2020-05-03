96d3640216be6d9adcfd79f45bd339f37d6f0268
Thiago Lima <thitheguy@gmail.com>
2020-05-02 23:29:40 -0300

    Adding deletePost Mutation;

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import {
     GraphQLServer
 } from 'graphql-yoga'
 import uuidv4 from 'uuid/v4'
+import {findArgChanges} from 'graphql/utilities/findBreakingChanges'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
@@ -81,6 +82,7 @@ const typeDefs = `
         createUser(data: CreateUserInput): User!
         deleteUser(id: ID!): User!
         createPost(data: CreatePostInput): Post!
+        deletePost(id: ID!): Post!
         createComment(data: CreateCommentInput): Comment!
     }
 
@@ -250,6 +252,20 @@ const resolvers = {
             posts.push(post)
 
             return post
+        },
+        deletePost(parent, args, ctx, info) {
+            const postIndex = posts.findIndex((post) => post.id === args.id)
+
+            if (postIndex === -1) {
+                throw new Error('Post not found')
+            }
+
+            const deletedPosts = posts.splice(postIndex, 1)
+
+            comments = comments.filter((comment) => comment.post !== args.id)
+
+            return deletedPosts[0]
+
         },
         createComment(parent, args, ctx, info) {
             const userExists = users.some((user) => user.id === args.data.author)
