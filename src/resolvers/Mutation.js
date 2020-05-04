4a27a4b071513806f38012d86b661fbc42a73b9f
Thiago Lima <thitheguy@gmail.com>
2020-05-03 21:42:36 -0300

    Adding Comment Subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -121,7 +121,7 @@ const Mutation = {
         return post
 
     },
-    createComment(parent, args, { db }, info) {
+    createComment(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
         const postExists = db.posts.some((post) => post.id === args.data.post && post.published)
 
@@ -135,6 +135,7 @@ const Mutation = {
         }
 
         db.comments.push(comment)
+        pubsub.publish(`comment ${args.data.post}`, { comment })
 
         return comment
 
