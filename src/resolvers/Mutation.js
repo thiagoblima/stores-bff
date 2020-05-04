5115117efbe394bf4d0cc34ac3ee42841d1116c9
Thiago Lima <thitheguy@gmail.com>
2020-05-03 22:45:09 -0300

    Adding Post Subscription/Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -83,7 +83,12 @@ const Mutation = {
         db.posts.push(post)
 
         if (args.data.published) {
-            pubsub.publish('post', { post })
+            pubsub.publish('post', { 
+                post: {
+                    mutation: 'CREATED',
+                    data: post
+                }
+             })
         }
 
         return post
