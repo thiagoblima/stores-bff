d4a273fa6c42a6df371558094f5bd238a7a6697c
Thiago Lima <thitheguy@gmail.com>
2020-05-03 22:26:52 -0300

    Adding Post Subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -68,7 +68,7 @@ const Mutation = {
 
        return user
     },
-    createPost(parent, args, { db }, info) {
+    createPost(parent, args, { db, pubsub }, info) {
         const userExists = db.users.some((user) => user.id === args.data.author)
 
         if (!userExists) {
@@ -81,6 +81,10 @@ const Mutation = {
         }
 
         db.posts.push(post)
+
+        if (args.data.published) {
+            pubsub.publish('post', { post })
+        }
 
         return post
     },
--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -23,6 +23,12 @@ const Subscription = {
 
             return pubsub.asyncIterator(`comment ${postId}`)
         }
+    },
+    post: {
+        subscribe(parent, args, { pubsub }, info) {
+            return pubsub.asyncIterator('post') 
+
+        }
     }
 }
 
